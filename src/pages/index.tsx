6a3204488a74ffa6ea6d2de673e083f00e3ea2bf
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2021-06-01 22:36:50 +0900

    [update] ディレクトリ構成変更、不要ファイル削除

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next'
+import Head from 'next/head';
+
+const Index: NextPage<void> = () => (
+  <>
+    <Head>
+        <title>トップページ</title>
+    </Head>
+    <div>
+      トップページ
+    </div>
+  </>
+);
+
+export default Index;
