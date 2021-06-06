e906f76485a32145442a727c39383b6fda75f20f
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2021-06-07 04:59:45 +0900

    [update] react query導入&サンプルコード追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 const MyApp:NextPage<AppProps> = ({ Component }) => (
-  <ChakraProvider>
-    <Component />
-  </ChakraProvider>
+  <QueryClientProvider client={queryClient}>
+    <ChakraProvider>
+      <Component />
+    </ChakraProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
 
 export default MyApp;
